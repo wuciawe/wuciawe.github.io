228777b473361c1d668f48590a1a74d9a72f72a8
z620 <h.seallstar@gmail.com>
2014-08-13 20:31:02 +0800

    update from local

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -2,6 +2,6 @@
  * Created by jwhu on 14-8-13.
  */
 
-$(document.links).filter(function () {
-    return this.hostname != window.location.hostname;
-}).attr('target', '_blank');
+//$(document.links).filter(function () {
+//    return this.hostname != window.location.hostname;
+//}).attr('target', '_blank');
