7c8ab6fdd9ad1fe221c974a88dce2f5079bacd75
z620 <h.seallstar@gmail.com>
2014-08-15 15:53:21 +0800

    navbar is fixed on top, js avoids overlapping

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -2,19 +2,8 @@
  * Created by jwhu on 14-8-13.
  */
 
-
 jQuery.noConflict();
 jQuery(document).ready(function(){
-//    var backToTopTxt = "▲", backToTopEle = jQuery('<div class="backToTop col-sm-offset-10 pull-right"></div>').appendTo(jQuery("body")).text(backToTopTxt).attr("title","Back to top").click(function() {
-//        jQuery("html, body").animate({ scrollTop: 0 }, 120);
-//    }), backToTopFun = function() {
-//        var st = jQuery(document).scrollTop(), winh = jQuery(window).height();
-//        (st > 200)? backToTopEle.show(): backToTopEle.hide();
-//        //IE6下的定位
-//        if (!window.XMLHttpRequest) {
-//            backToTopEle.css("top", st + winh - 166);
-//        }
-//    };
 
     var backToTopEle = jQuery('<div class="backToTop col-sm-offset-10 pull-right"><img src="/assets/image/backtotop.png"></div>').appendTo(jQuery("body")).click(function() {
         jQuery("html, body").animate({ scrollTop: 0 }, 120);
@@ -30,6 +19,15 @@ jQuery(document).ready(function(){
     backToTopEle.hide();
     jQuery(window).bind("scroll", backToTopFun);
     jQuery('div.main a,div.pic a').attr('target', '_blank');
+
+    jQuery(window).resize(function () {
+        jQuery('body').css('padding-top', parseInt(jQuery('nav').css("height"))+10);
+    });
+
+    jQuery(window).resize();
+    jQuery(window).load(function () {
+        jQuery('body').css('padding-top', parseInt(jQuery('nav').css("height"))+10);
+    });
 });
 
 jQuery("#search-form").submit(function(){
