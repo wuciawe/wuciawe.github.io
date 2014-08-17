839c254d907fb911cb2c3a828798a43e835a50a6
server-239 <h.seallstar@gmail.com>
2014-08-17 17:01:05 +0800

    chanage back to navbar-static-top, update backtotop

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -5,10 +5,18 @@
 jQuery.noConflict();
 jQuery(document).ready(function(){
 
-    var backToTopEle = jQuery('<div class="backToTop col-sm-offset-10 pull-right"><img src="/assets/image/backtotop.png"></div>').appendTo(jQuery("body")).click(function() {
+    var vWindow = jQuery(window);
+    var vBody = jQuery('body');
+    var vNav = jQuery('nav');
+    var vSearch = jQuery('#search-form');
+
+    var backToTopEle = jQuery('.backToTop');
+    backToTopEle.click(function() {
         jQuery("html, body").animate({ scrollTop: 0 }, 120);
-    }), backToTopFun = function() {
-        var st = jQuery(document).scrollTop(), winh = jQuery(window).height();
+    });
+
+    var backToTopFun = function() {
+        var st = jQuery(document).scrollTop(), winh = vWindow.height();
         (st > 200)? backToTopEle.show(): backToTopEle.hide();
         //IE6下的定位
         if (!window.XMLHttpRequest) {
@@ -17,20 +25,20 @@ jQuery(document).ready(function(){
     };
 
     backToTopEle.hide();
-    jQuery(window).bind("scroll", backToTopFun);
+    vWindow.bind("scroll", backToTopFun);
     jQuery('div.main a,div.pic a').attr('target', '_blank');
 
-    jQuery(window).resize(function () {
-        jQuery('body').css('padding-top', parseInt(jQuery('nav').css("height"))+10);
-    });
+    //vWindow.resize(function () {
+    //    vBody.css('padding-top', parseInt(vNav.css("height"))+10);
+    //});
 
-    jQuery(window).resize();
-    jQuery(window).load(function () {
-        jQuery('body').css('padding-top', parseInt(jQuery('nav').css("height"))+10);
+    //vWindow.load(function () {
+    //    vBody.css('padding-top', parseInt(vNav.css("height"))+10);
+    //});
+
+    vSearch.submit(function(){
+        var query = document.getElementById("google-search").value;
+        window.open("http://google.com/search?q=" + query+ "%20site:" + "wuciawe.github.io");
     });
 });
 
-jQuery("#search-form").submit(function(){
-    var query = document.getElementById("google-search").value;
-    window.open("http://google.com/search?q=" + query+ "%20site:" + "wuciawe.github.io");
-});
